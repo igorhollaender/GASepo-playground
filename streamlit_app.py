8f49af26b44b0a41f2ed75e43789a62c98f4110e
Igor Holländer <igorhollaender@users.noreply.github.com>
2025-08-12 14:04:47 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ from streamlit_drawable_canvas import st_canvas
 GASepoPG_version = "250812f"
 
 def main():
+
+    # ---- get image
+
     gel_image_uploaded = st.file_uploader(
         "Upload a gel image file", 
         type=["tif","tiff"])
@@ -34,12 +37,35 @@ def main():
 
             # Convert to RGB if needed
             if len(gel_image_display.shape) == 2:
-                st.image(gel_image_display, caption="16-bit Grayscale Image", use_container_width=True)
+                st.image(gel_image_display, caption="16-bit Grayscale Image", width=300, use_container_width=False)
             else:
-                st.image(cv2.cvtColor(gel_image_display, cv2.COLOR_BGR2RGB), caption="16-bit Color Image", use_container_width=True)
+                st.image(cv2.cvtColor(gel_image_display, cv2.COLOR_BGR2RGB), caption="16-bit Color Image", width=300, use_container_width=False)
     else:
         st.write("Please upload a gel image file to proceed")
     
+    # prepare canvas_1 for drawing
+
+    canvas_1_height, canvas_1_width = gel_image_display.shape[:2]
+    # Scale canvas for display if image is too wide
+    if canvas_1_width > 800:  #IH250812 HEURISTIC
+        aspect_ratio = canvas_1_height / canvas_1_width
+        canvas_1_width = 800
+        canvas_1_height = int(canvas_1_width * aspect_ratio)
+
+    canvas_1 = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",
+        stroke_width=2,
+        stroke_color="#FFA500",
+        background_image=gel_image_display,
+        # update_streamlit=realtime_update, # Enable real-time updates only for the 3rd point
+        height=canvas_1_height,  #IH250812 NOT ACTIVE FOR NOW
+        width=canvas_1_width,   # IH250812 NOT ACTIVE FOR NOW
+        use_container_width=True,
+        drawing_mode="point",
+        # initial_drawing=initial_drawing if initial_drawing["objects"] else None,
+        key="canvas_1",
+    )
+
 
 #----------
 if __name__ == "__main__":
