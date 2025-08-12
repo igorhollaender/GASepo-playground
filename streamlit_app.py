d1e605fed8c29141e3ec162dfc849e1b53ab4c7e
Igor Holländer <igorhollaender@users.noreply.github.com>
2025-08-12 13:16:00 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@
 #
 
 import cv2
+import io
 import numpy as np
 from PIL import Image
 import streamlit as st
@@ -21,16 +22,20 @@ st.sidebar.header("GASepo Playground Controls")
 # Upload and prepare image data
 gel_image_uploaded = st.file_uploader(
     "Upload a gel image file", 
-    type=["jpg", "jpeg", "png","tiff","tif"])
+    type=["tif","tiff","png","jpg", "jpeg"])
 if gel_image_uploaded is not None:
-    st.image(gel_image_uploaded, caption="Gel Image", use_container_width=True)
-    gel_image_PIL = Image.open(gel_image_uploaded)
+    gel_image_bytes = gel_image_uploaded.getvalue()
+    gel_image_PIL = Image.open(io.Bytes(gel_image_bytes))
     gel_image_original = np.array(gel_image_PIL)   
+
     # convert RGBA/RGB to BGR for OpenCV
     if gel_image_PIL.mode == 'RGBA':  # IH250812 check this!!!
         gel_image_BGR = cv2.cvtColor(np.array(gel_image_PIL), cv2.COLOR_RGBA2BGR)
     else:
         gel_image_BGR = cv2.cvtColor(np.array(gel_image_PIL), cv2.COLOR_RGB2BGR)
+    
+    st.image(gel_image_PIL, caption="Gel Image", use_container_width=True)
+    
 else:
     st.write("Please upload a gel image file to proceed")
     
