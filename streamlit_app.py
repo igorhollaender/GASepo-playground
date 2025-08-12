8db052b7cc76692a0c776978297f1e4705562403
Igor Holländer <igorhollaender@users.noreply.github.com>
2025-08-12 10:54:37 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
+#
+#   G A S e p o     P l a y g r o u n d
+#
+#   Last Update: IH250812
+#
+#
+
 import streamlit as st
+import cv2
+from PIL import Image
+from streamlit_drawable_canvas import st_canvas
 
 st.title("GASepo Playground Ver.250812a")
 st.write(
@@ -8,7 +18,7 @@ gel_image_uploaded = st.file_uploader(
     "Upload a gel image file", 
     type=["jpg", "jpeg", "png","tiff","tif"])
 if gel_image_uploaded is not None:
-    st.image(gel_image_uploaded, caption="Gel Image", use_column_width=True)
+    st.image(gel_image_uploaded, caption="Gel Image", use_container_width=True)
 else:
     st.write("Please upload a gel image file to proceed")
     
