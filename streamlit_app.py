134c1e10f0b8a435778b03f90f55bbecea411a1b
Igor Holländer <igorhollaender@users.noreply.github.com>
2025-08-12 10:43:08 +0000

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app VER1")
+st.title("GASepo Playground Ver.250812a")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+gel_image_uploaded = st.file_uploader(
+    "Upload a gel image file", 
+    type=["jpg", "jpeg", "png","tiff","tif"])
+if gel_image_uploaded is not None:
+    st.image(gel_image_uploaded, caption="Gel Image", use_column_width=True)
+else:
+    st.write("Please upload a gel image file to proceed")
+    
